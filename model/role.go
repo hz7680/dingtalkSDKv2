628c73ec1cf54707d4d29deaa5ac740c1c4e41c6
1109354657 <lvewg@vip.qq.com>
2021-01-19 13:56:07 +0800

    获取用户组+用户id列表

--- model/role.go
+++ model/role.go
@@ -5,14 +5,48 @@ type GetRoleGroupRequest struct {
 }
 
 type GetRoleGroupResponse struct {
-	Errcode int    `json:"errcode"`
-	Errmsg  string `json:"errmsg"`
+	Errcode   int    `json:"errcode"`
+	Errmsg    string `json:"errmsg"`
 	RequestId string `json:"request_id"`
-	RoleGroup struct{
+	RoleGroup struct {
 		GroupName string `json:"group_name"`
-		Roles []struct{
-			RoleId int `json:"role_id"`
+		Roles     []struct {
+			RoleId   int    `json:"role_id"`
 			RoleName string `json:"role_name"`
 		} `json:"roles"`
 	} `json:"role_group"`
+}
+
+type GetRoleUserRequest struct {
+	RoleId int `json:"role_id"`
+}
+
+type GetRoleUserResponse struct {
+	Errcode   int    `json:"errcode"`
+	Errmsg    string `json:"errmsg"`
+	RequestId string `json:"request_id"`
+	Result    struct {
+		HasMore    bool `json:"hasMore"`
+		NextCursor int  `json:"nextCursor"`
+		User       []struct {
+			UserId string `json:"userid"`
+			Name   string `json:"name"`
+		} `json:"list"`
+	} `json:"result"`
+}
+
+type RoleListResponse struct {
+	Errcode int    `json:"errcode"`
+	Errmsg  string `json:"errmsg"`
+	Result  struct {
+		HasMore bool `json:"has_more"`
+		List    []struct {
+			Name    string `json:"name"`
+			GroupId int    `json:"groupId"`
+			Roles   []struct {
+				Id   int    `json:"id"`
+				Name string `json:"name"`
+			} `json:"roles"`
+		} `json:"list"`
+	} `json:"result"`
 }
