51fabed1cec6b2f18e9727aff25239065e06f051
华朕 <346187456@qq.com>
2021-01-19 16:45:53 +0800

    no message

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"dingtalkSdkV2/client"
+	"dingtalkSdkV2/model"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -28,8 +32,40 @@ func main() {
 	//fmt.Println(err)
 
 	http.HandleFunc("/callback", func(writer http.ResponseWriter, request *http.Request) {
-
+		all, err := ioutil.ReadAll(request.Body)
+		if err != nil {
+			fmt.Println("got an error when read body : ", err)
+			return
+		}
+		temp := string(all)
+		fmt.Println(temp)
+		var req model.CallBackRequest
+		err = json.Unmarshal(all, &req)
+		if err != nil {
+			fmt.Println("got an error when json unmarshal : ", err)
+			return
+		}
+		req.Nonce = request.URL.Query().Get("nonce")
+		req.Signature = request.URL.Query().Get("signature")
+		req.TimeStamp = request.URL.Query().Get("timestamp")
+		decryptMsg, err := dingtalkClient.Decrypt(&req)
+		if err != nil {
+			fmt.Println("got an error when decrypt : ", err)
+		}
+		fmt.Println(decryptMsg)
+		//if decryptMsg["EventType"] == "check_url"{
+		resp, err := dingtalkClient.GenerateSuccessResp(&req)
+		if err != nil {
+			fmt.Println("got an error when generate success response :", err)
+		}
+		marshalBytes, err := json.Marshal(resp)
+		writer.Write(marshalBytes)
+		//}
 	})
-	http.ListenAndServe("8001",nil)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hello world"))
+	})
+	err := http.ListenAndServe(":8001", nil)
+	fmt.Println(err)
 
 }
